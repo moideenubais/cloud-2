fdd118810fd9113128080b01d2fee524804f1766
Vasudev-UpMyRanks <115608959+Vasudev-UpMyRanks@users.noreply.github.com>
2023-05-31 12:26:09 +0530

    done with overlappig issue if smaller screen

--- src/MovingDivs.jsx
+++ src/MovingDivs.jsx
@@ -12,29 +12,19 @@ const MovingDivs = () => {
 
   const generateDivs = () => {
     const newDivs = [];
-    const containerWidth = window.innerWidth;
-    const containerHeight = window.innerHeight;
 
     // Number of divs and their initial positions
-    const numDivs = 20;
+    const numDivs = 10;
     const initialPositions = [
       { top: 10 },
       { top: 150 },
       { top: 350 },
       // Add more initial positions as needed
     ];
-    const animationNames = [
-      "m1",
-      "m2",
-      "m3",
-      // Add more initial positions as needed
-    ];
 
     for (let i = 0; i < numDivs; i++) {
       const div = {
         id: i,
-        x: initialPositions[i % initialPositions.length].x + i * 100,
-        y: initialPositions[i % initialPositions.length].y,
         top: initialPositions[i % initialPositions.length].top,
         animationDelay: `${i +2}s`,
 
@@ -47,7 +37,10 @@ const MovingDivs = () => {
   return (
     <div className="moving-divs-container">
       {divs.map((div) => (
-        <div key={div.id} className="moving-div" style={{ top: div.top,animationDelay:div.animationDelay }}>
+        <div key={div.id} className="moving-div" style={{ top: div.top,animationDelay:div.animationDelay, backgroundImage: `url(${process.env.PUBLIC_URL}/cloud.png)`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center', }}>
           {div.id+1}
         </div>
       ))}
