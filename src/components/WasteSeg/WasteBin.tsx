1a4cf95f7eacd3342e0d055a4f437ad6f3ec2053
Vasudev-UpMyRanks <115608959+Vasudev-UpMyRanks@users.noreply.github.com>
2023-07-01 11:03:59 +0530

    preloading in waste seg

--- src/components/WasteSeg/WasteBin.tsx
+++ src/components/WasteSeg/WasteBin.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import classes from "./WasteBin.module.css";
 import { useDrop } from "react-dnd";
 import { BinType, ImageVaiations } from "./types";
@@ -22,6 +22,7 @@ const WasteBin: React.FC<Props> = ({
   onDrop,
   draggingItemType,
 }) => {
+  const [backgroundImage, setBackgroundImage] = useState<string|null>(null)
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [{ canDrop, isOver }, drop] = useDrop({
     accept: "trash",
@@ -34,80 +35,109 @@ const WasteBin: React.FC<Props> = ({
       canDrop: monitor.canDrop(),
     }),
   });
-  let backgroundImage = `url(./images/wasteSeg/trash/${imageClosed}.png)`;
-  if (isOver) {
+
+  const changeBackgroundImage = async ()=>{
     if (type === BinType.Dry) {
-      console.log("ttt type ", type, draggingItemType);
-      if (draggingItemType === BinType.Dry)
-        backgroundImage = `url(./images/wasteSeg/trash/${imageOpened}.png)`;
-      else if (draggingItemType === BinType.E_Waste)
-        backgroundImage = `url(./images/wasteSeg/trash/${images.eToDry}.png)`;
-      else if (draggingItemType === BinType.Hazardous)
-        backgroundImage = `url(./images/wasteSeg/trash/${images.hToDry}.png)`;
-      else if (draggingItemType === BinType.Wet)
-        backgroundImage = `url(./images/wasteSeg/trash/${images.wToDry}.png)`;
-      else if (draggingItemType === BinType.Sanitary)
-        backgroundImage = `url(./images/wasteSeg/trash/${images.sToDry}.png)`;
+      if (draggingItemType === BinType.Dry){
+        await importAndSetBackground(imageOpened)
+      }
+      // else if (draggingItemType === BinType.E_Waste)
+      //   backgroundImage = `url(./images/wasteSeg/trash/${images.eToDry}.png)`;
+      // else if (draggingItemType === BinType.Hazardous)
+      //   backgroundImage = `url(./images/wasteSeg/trash/${images.hToDry}.png)`;
+      // else if (draggingItemType === BinType.Wet)
+      //   backgroundImage = `url(./images/wasteSeg/trash/${images.wToDry}.png)`;
+      // else if (draggingItemType === BinType.Sanitary)
+      //   backgroundImage = `url(./images/wasteSeg/trash/${images.sToDry}.png)`;
     }
-    if (type === BinType.Wet) {
-      if (draggingItemType === BinType.Dry)
-        backgroundImage = `url(./images/wasteSeg/trash/${images.dToWet}.png)`;
-      else if (draggingItemType === BinType.E_Waste)
-        backgroundImage = `url(./images/wasteSeg/trash/${images.eToWet}.png)`;
-      else if (draggingItemType === BinType.Hazardous)
-        backgroundImage = `url(./images/wasteSeg/trash/${images.hToWet}.png)`;
-      else if (draggingItemType === BinType.Wet)
-        backgroundImage = `url(./images/wasteSeg/trash/${imageOpened}.png)`;
-      else if (draggingItemType === BinType.Sanitary)
-        backgroundImage = `url(./images/wasteSeg/trash/${images.sToWet}.png)`;
-    }
-    if (type === BinType.E_Waste) {
-      if (draggingItemType === BinType.Dry)
-        backgroundImage = `url(./images/wasteSeg/trash/${images.dToE}.png)`;
-      else if (draggingItemType === BinType.E_Waste)
-        backgroundImage = `url(./images/wasteSeg/trash/${imageOpened}.png)`;
-      else if (draggingItemType === BinType.Hazardous)
-        backgroundImage = `url(./images/wasteSeg/trash/${images.hToE}.png)`;
-      else if (draggingItemType === BinType.Wet)
-        backgroundImage = `url(./images/wasteSeg/trash/${images.wToE}.png)`;
-      else if (draggingItemType === BinType.Sanitary)
-        backgroundImage = `url(./images/wasteSeg/trash/${images.sToE}.png)`;
-    }
-    if (type === BinType.Sanitary) {
-      if (draggingItemType === BinType.Dry)
-        backgroundImage = `url(./images/wasteSeg/trash/${images.dToSan}.png)`;
-      else if (draggingItemType === BinType.E_Waste)
-        backgroundImage = `url(./images/wasteSeg/trash/${images.eToSan}.png)`;
-      else if (draggingItemType === BinType.Hazardous)
-        backgroundImage = `url(./images/wasteSeg/trash/${images.hToSan}.png)`;
-      else if (draggingItemType === BinType.Wet)
-        backgroundImage = `url(./images/wasteSeg/trash/${images.wToSan}.png)`;
-      else if (draggingItemType === BinType.Sanitary)
-        backgroundImage = `url(./images/wasteSeg/trash/${imageOpened}.png)`;
-    }
-    if (type === BinType.Hazardous) {
-      if (draggingItemType === BinType.Dry)
-        backgroundImage = `url(./images/wasteSeg/trash/${images.dToHaz}.png)`;
-      else if (draggingItemType === BinType.E_Waste)
-        backgroundImage = `url(./images/wasteSeg/trash/${images.eToHaz}.png)`;
-      else if (draggingItemType === BinType.Hazardous)
-        backgroundImage = `url(./images/wasteSeg/trash/${imageOpened}.png)`;
-      else if (draggingItemType === BinType.Wet)
-        backgroundImage = `url(./images/wasteSeg/trash/${images.wToHaz}.png)`;
-      else if (draggingItemType === BinType.Sanitary)
-        backgroundImage = `url(./images/wasteSeg/trash/${images.sToHaz}.png)`;
-    }
+    // if (type === BinType.Wet) {
+    //   if (draggingItemType === BinType.Dry)
+    //     backgroundImage = `url(./images/wasteSeg/trash/${images.dToWet}.png)`;
+    //   else if (draggingItemType === BinType.E_Waste)
+    //     backgroundImage = `url(./images/wasteSeg/trash/${images.eToWet}.png)`;
+    //   else if (draggingItemType === BinType.Hazardous)
+    //     backgroundImage = `url(./images/wasteSeg/trash/${images.hToWet}.png)`;
+    //   else if (draggingItemType === BinType.Wet)
+    //     backgroundImage = `url(./images/wasteSeg/trash/${imageOpened}.png)`;
+    //   else if (draggingItemType === BinType.Sanitary)
+    //     backgroundImage = `url(./images/wasteSeg/trash/${images.sToWet}.png)`;
+    // }
+    // if (type === BinType.E_Waste) {
+    //   if (draggingItemType === BinType.Dry)
+    //     backgroundImage = `url(./images/wasteSeg/trash/${images.dToE}.png)`;
+    //   else if (draggingItemType === BinType.E_Waste)
+    //     backgroundImage = `url(./images/wasteSeg/trash/${imageOpened}.png)`;
+    //   else if (draggingItemType === BinType.Hazardous)
+    //     backgroundImage = `url(./images/wasteSeg/trash/${images.hToE}.png)`;
+    //   else if (draggingItemType === BinType.Wet)
+    //     backgroundImage = `url(./images/wasteSeg/trash/${images.wToE}.png)`;
+    //   else if (draggingItemType === BinType.Sanitary)
+    //     backgroundImage = `url(./images/wasteSeg/trash/${images.sToE}.png)`;
+    // }
+    // if (type === BinType.Sanitary) {
+    //   if (draggingItemType === BinType.Dry)
+    //     backgroundImage = `url(./images/wasteSeg/trash/${images.dToSan}.png)`;
+    //   else if (draggingItemType === BinType.E_Waste)
+    //     backgroundImage = `url(./images/wasteSeg/trash/${images.eToSan}.png)`;
+    //   else if (draggingItemType === BinType.Hazardous)
+    //     backgroundImage = `url(./images/wasteSeg/trash/${images.hToSan}.png)`;
+    //   else if (draggingItemType === BinType.Wet)
+    //     backgroundImage = `url(./images/wasteSeg/trash/${images.wToSan}.png)`;
+    //   else if (draggingItemType === BinType.Sanitary)
+    //     backgroundImage = `url(./images/wasteSeg/trash/${imageOpened}.png)`;
+    // }
+    // if (type === BinType.Hazardous) {
+    //   if (draggingItemType === BinType.Dry)
+    //     backgroundImage = `url(./images/wasteSeg/trash/${images.dToHaz}.png)`;
+    //   else if (draggingItemType === BinType.E_Waste)
+    //     backgroundImage = `url(./images/wasteSeg/trash/${images.eToHaz}.png)`;
+    //   else if (draggingItemType === BinType.Hazardous)
+    //     backgroundImage = `url(./images/wasteSeg/trash/${imageOpened}.png)`;
+    //   else if (draggingItemType === BinType.Wet)
+    //     backgroundImage = `url(./images/wasteSeg/trash/${images.wToHaz}.png)`;
+    //   else if (draggingItemType === BinType.Sanitary)
+    //     backgroundImage = `url(./images/wasteSeg/trash/${images.sToHaz}.png)`;
+    // }
   }
 
-  return (
+  useEffect(() => {
+    
+   if (isOver) {
+    changeBackgroundImage();
+  } else {importAndSetBackground(imageClosed)}
+  }, [isOver])
+  
+
+  useEffect(() => {
+    importAndSetBackground(imageClosed)
+  
+   
+  }, [])
+
+  const loadBackgroundImage = async ()=>{
+    const backImage = await import(`../../static/wasteSeg/trash/${imageClosed}.png`)
+    setBackgroundImage(`url(${backImage.default})`)
+    // let img = require(`../../static/wasteSeg/trash/${imageClosed}.png`)
+  // console.log("ttt in",img)
+ 
+  }
+
+  const importAndSetBackground = async (path:string) => {
+    const backImage = await import(`../../static/wasteSeg/trash/${path}.png`)
+    setBackgroundImage(`url(${backImage.default})`)
+  }
+  
+  
+
+  return backgroundImage? (
     <div
       ref={drop}
       style={{
         backgroundImage: backgroundImage,
       }}
       className={classes.wasteBin}
-    ></div>
-  );
+    ></div>):<div></div>
+  ;
 };
 
 export default WasteBin;
--- src/components/WasteSeg/WasteSeg.tsx
+++ src/components/WasteSeg/WasteSeg.tsx
@@ -2,10 +2,7 @@ import React, { useEffect, useState } from "react";
 import WasteItem from "./WasteItem";
 import WasteBin from "./WasteBin";
 import classes from "./WasteSeg.module.css";
-import {
-  E_Waste_ITEMS,
-  WASTE_BINS,
-} from "../../constants/wasteSeg";
+import { E_Waste_ITEMS, WASTE_BINS } from "../../constants/wasteSeg";
 import { DRY_ITEMS } from "../../constants/wasteSeg";
 import { WasteItemType } from "./types";
 import Modal from "../modal/Modal";
@@ -15,14 +12,78 @@ const WasteSeg: React.FC = () => {
   const [wasteBins, setWasteBins] = useState(WASTE_BINS);
   const [wasteItems, setWasteItems] = useState<WasteItemType[]>([]);
   const [showModal, setShowModal] = useState(false);
-  const [draggingItemType, setDraggingItemType] = useState('')
+  const [draggingItemType, setDraggingItemType] = useState("");
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const combinedArray = DRY_ITEMS.concat(
-      E_Waste_ITEMS
-    );
-    setWasteItems(shuffleArray(combinedArray));
+    const combinedArray = DRY_ITEMS.concat(E_Waste_ITEMS);
+    setWasteItems(shuffleArray(combinedArray).slice(0, 10));
+
+    preloadImages();
   }, []);
+
+  // function preloadImages(imageUrls: any) {
+  //   return new Promise((resolve, reject) => {
+  //     const loadedImages: any = {};
+  //     let imagesLoaded = 0;
+
+  //     function imageLoaded() {
+  //       imagesLoaded++;
+  //       if (imagesLoaded === imageUrls.length) {
+  //         resolve(loadedImages);
+  //       }
+  //     }
+
+  //     for (const url of imageUrls) {
+  //       const img = new Image();
+  //       img.onload = imageLoaded;
+  //       img.onerror = imageLoaded;
+  //       img.src = url;
+
+  //     }
+  //   });
+  // }
+
+  const preloadImages = async () => {
+    const images = [await import(`../../static/wasteSeg/background.png`)];
+    let imagesLoaded = 0;
+
+    function imageLoaded() {
+      imagesLoaded++;
+      if (imagesLoaded === images.length) {
+        setLoading(false);
+      }
+    }
+
+    for (const bin of wasteBins) {
+      images.push(
+        await import(`../../static/wasteSeg/trash/${bin.imageOpened}.png`)
+      );
+      images.push(
+        await import(`../../static/wasteSeg/trash/${bin.imageClosed}.png`)
+      );
+    }
+
+    for (const image of images) {
+      const img = new Image();
+      img.onload = imageLoaded;
+      img.onerror = imageLoaded;
+      img.src = image.default;
+    }
+
+    // const dryClosed = require(`../../static/wasteSeg/trash/dryClosed.png`);
+    // console.log("ttt ",backgroundImage,dryClosed)
+    // const img = new Image();
+    // const img2 = new Image();
+
+    // img.src = backgroundImage;
+    // img2.src = dryClosed
+    // img2.onload = () =>{setLoading(false)}
+  };
+
+  const backgroundStyle = loading
+    ? { background: "linear-gradient(45deg, #ff6699, #66ccff)" }
+    : {};
 
   const handleOnDrop = (id: number) => {
     console.log("wasteItem", wasteItems, id);
@@ -45,38 +106,45 @@ const WasteSeg: React.FC = () => {
     return shuffledArray;
   };
   return (
-    <div className={classes.gameContainer}>
-      <div className={classes.wasteBinContainer}>
-        {wasteBins.map((wasteBin) => {
-          return (
-            <WasteBin
-              key={wasteBin.id}
-              type={wasteBin.type}
-              id={wasteBin.id}
-              imageOpened={wasteBin.imageOpened}
-              imageClosed={wasteBin.imageClosed}
-              onDrop={handleOnDrop}
-              images={wasteBin.images}
-              draggingItemType={draggingItemType}
-            />
-          );
-        })}
-      </div>
-      <div className={classes.wasteBackground}>
-        <div className={classes.wasteItemContainer}>
-          {wasteItems.map((wasteItem) => {
-            return (
-              <WasteItem
-                key={wasteItem.id}
-                url={wasteItem.image}
-                id={wasteItem.id}
-                type={wasteItem.type}
-                setDraggingItemType={setDraggingItemType}
-              />
-            );
-          })}
-        </div>
-      </div>
+    <div className={classes.gameContainer} style={backgroundStyle}>
+      {loading ? (
+        "Loading..."
+      ) : (
+        <>
+          <div className={classes.wasteBinContainer}>
+            {wasteBins.map((wasteBin) => {
+              return (
+                <WasteBin
+                  key={wasteBin.id}
+                  type={wasteBin.type}
+                  id={wasteBin.id}
+                  imageOpened={wasteBin.imageOpened}
+                  imageClosed={wasteBin.imageClosed}
+                  onDrop={handleOnDrop}
+                  images={wasteBin.images}
+                  draggingItemType={draggingItemType}
+                />
+              );
+            })}
+          </div>
+          <div className={classes.wasteBackground}>
+            <div className={classes.wasteItemContainer}>
+              {wasteItems.map((wasteItem) => {
+                return (
+                  <WasteItem
+                    key={wasteItem.id}
+                    url={wasteItem.image}
+                    id={wasteItem.id}
+                    type={wasteItem.type}
+                    setDraggingItemType={setDraggingItemType}
+                  />
+                );
+              })}
+            </div>
+          </div>
+        </>
+      )}
+
       {showModal && <Modal message="Task Completed" />}
     </div>
   );
